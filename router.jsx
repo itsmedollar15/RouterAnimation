bfbf848cc1979b3e50974ba438ed9f6421562d09
Sameer Ahmed Shaikh <99016172+Sameer0425@users.noreply.github.com>
2024-02-19 23:00:32 +0530

    Create router.jsx

---
+++ router.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { FadeInAnimation } from 'router-animation';
+
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <FadeInAnimation>
+            <HomePage />
+          </FadeInAnimation>
+        </Route>
+        <Route path="/about">
+          <FadeInAnimation>
+            <AboutPage />
+          </FadeInAnimation>
+        </Route>
+        <Route path="/contact">
+          <FadeInAnimation>
+            <ContactPage />
+          </FadeInAnimation>
+        </Route>
+      </Switch>
+    </Router>
+  );
+};
+
+export default App;
